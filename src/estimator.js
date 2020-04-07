da5cd0d3dc5f99f72bf16e50002830371dc360e4
yegow <allanyego05@gmail.com>
2020-04-07 19:53:12 +0300

    Completed challenge one requirements

--- src/estimator.js
+++ src/estimator.js
@@ -1,3 +1,29 @@
-const covid19ImpactEstimator = (data) => data;
+import infectionsByTime from './infectionsByTime';
+
+function est(severe, {
+  reportedCases,
+  timeToElapse,
+  periodType = 'days'
+}) {
+  const currentlyInfected = severe ? reportedCases * 50 : reportedCases * 10;
+  const infectionsByRequestedTime = infectionsByTime(
+    timeToElapse,
+    currentlyInfected,
+    periodType
+  );
+
+  return {
+    currentlyInfected,
+    infectionsByRequestedTime
+  };
+}
+
+const covid19ImpactEstimator = function covid19ImpactEstimator(data) {
+  return {
+    data: { ...data },
+    impact: est(false, data),
+    severeImpact: est(true, data)
+  };
+};
 
 export default covid19ImpactEstimator;
