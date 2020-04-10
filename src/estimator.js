9153d390751ef5bdccef0f6cfd45476c29bef863
yegow <allanyego05@gmail.com>
2020-04-10 12:26:59 +0300

    Hospital beds! Arrrgh

--- src/estimator.js
+++ src/estimator.js
@@ -16,8 +16,8 @@ function est(severe, {
 
   const severeCasesByRequestedTime = Math.floor(infectionsByRequestedTime * 0.15);
 
-  const hospitalBedsByRequestedTime = Math.floor(totalHospitalBeds
-    * 0.35 - severeCasesByRequestedTime);
+  const hospitalBedsByRequestedTime = Math.floor(Math.floor(totalHospitalBeds
+    * 0.35) - infectionsByRequestedTime * 0.15);
 
   const casesForICUByRequestedTime = Math.floor(infectionsByRequestedTime * 0.05);
 
