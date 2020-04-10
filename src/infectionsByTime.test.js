7734294aa6e56c9707a9f65c07a61c41c3faf4d1
yegow <allanyego05@gmail.com>
2020-04-10 10:35:49 +0300

    Front end draft 1

--- src/infectionsByTime.test.js
+++ src/infectionsByTime.test.js
@@ -3,17 +3,17 @@ import infectionsByTime from './infectionsByTime';
 describe('infectionsByTime function', () => {
   it('should return correct estimates of days', () => {
     // Test case 100, 3days
-    const estimate = infectionsByTime(3, 100);
-    expect(estimate).toEqual(200);
+    const { infectionsByRequestedTime } = infectionsByTime(3, 100);
+    expect(infectionsByRequestedTime).toEqual(200);
   });
   it('should return correct estimates of weeks', () => {
     // Test case 100, 1week (7days)
-    const estimate = infectionsByTime(1, 100, 'weeks');
-    expect(estimate).toEqual(400);
+    const { infectionsByRequestedTime } = infectionsByTime(1, 100, 'weeks');
+    expect(infectionsByRequestedTime).toEqual(400);
   });
   it('should return correct estimates of months', () => {
     // Test case 100, 1month (30days)
-    const estimate = infectionsByTime(1, 100, 'months');
-    expect(estimate).toEqual(102400);
+    const { infectionsByRequestedTime } = infectionsByTime(1, 100, 'months');
+    expect(infectionsByRequestedTime).toEqual(102400);
   });
 });
