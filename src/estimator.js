bc78964e9c2e9fcd7ae4e2c7d9f073d2dd99e6f0
yegow <allanyego05@gmail.com>
2020-04-10 13:54:51 +0300

    On to challenge 3; ventilators

--- src/estimator.js
+++ src/estimator.js
@@ -21,8 +21,7 @@ function est(severe, {
 
   const casesForICUByRequestedTime = Math.trunc(infectionsByRequestedTime * 0.05);
 
-  const casesForVentilatorsByRequestedTime = `${infectionsByRequestedTime}, 
-    ${infectionsByRequestedTime * 0.02}`;
+  const casesForVentilatorsByRequestedTime = infectionsByRequestedTime * 0.02;
 
   const { avgDailyIncomePopulation, avgDailyIncomeInUSD } = region;
   const dollarsInFlight = Math.trunc(infectionsByRequestedTime
