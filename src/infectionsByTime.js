6021cb9cddc413fcef55a1bf0c7b8334fa5f8717
yegow <allanyego05@gmail.com>
2020-04-10 11:25:59 +0300

    So many floor division issues

--- src/infectionsByTime.js
+++ src/infectionsByTime.js
@@ -5,22 +5,19 @@ function infectionsByTime(duration, currentEstimate, periodType = 'days') {
   switch (periodType) {
     case 'weeks':
       days = duration * 7;
-      // factor = Math.floor(days / 3);
-      factor = days / 3;
+      factor = Math.floor(days / 3);
       break;
     case 'months':
       days = duration * 30;
-      // factor = Math.floor(days / 3);
-      factor = days / 3;
+      factor = Math.floor(days / 3);
       break;
     default:
-      // factor = Math.floor(duration / 3);
-      factor = duration / 3;
+      factor = Math.floor(duration / 3);
       break;
   }
 
   return {
-    infectionsByRequestedTime: Math.floor(currentEstimate * (2 ** factor)),
+    infectionsByRequestedTime: currentEstimate * (2 ** factor),
     days: days || duration
   };
 }
