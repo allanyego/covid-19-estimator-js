acd7ecfcea8c4b21d89314712a4ee96b735a16c2
Yego Allan <33445976+yegow@users.noreply.github.com>
2020-04-12 20:15:11 +0300

    Update infectionsByTime.js

--- src/infectionsByTime.js
+++ src/infectionsByTime.js
@@ -1,6 +1,7 @@
 function infectionsByTime(duration, currentEstimate, periodType = 'days') {
   let factor = null;
   let days = null;
+  console.log('commit prompt');
 
   switch (periodType) {
     case 'weeks':
